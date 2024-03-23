b9817fe834167c570f813dbb6083eaac28597434
Abdullokh Musaev <abdulloh.m@payme.uz>
2024-03-23 16:46:19 +0500

    use app run PORT from env

--- main.go
+++ main.go
@@ -1,31 +1,33 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/abdulloh76/storage-manager/pkg/domain"
 	"github.com/abdulloh76/storage-manager/pkg/handlers"
 	"github.com/abdulloh76/storage-manager/pkg/store"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	portFlag := flag.Int("port", 8080, "listening port")
-	flag.Parse()
-	PORT := fmt.Sprintf(":%d", *portFlag)
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
 
-	DATABASE_URL := "postgres://user:password@localhost:5432/storage"
+	PORT := os.Getenv("PORT")
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+
 	postgresMetadataStore := store.NewPostgresDBStore(DATABASE_URL)
-
 	objectDomain := domain.NewObjectsDomain(postgresMetadataStore)
-
 	HttpHandler := handlers.NewHttpHandler(objectDomain)
 
 	handlers.RegisterHandlers(HttpHandler)
 
 	fmt.Println("Server running on http://localhost" + PORT)
-	err := http.ListenAndServe(PORT, nil)
+	err = http.ListenAndServe(":"+PORT, nil)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
